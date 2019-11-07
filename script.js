bd2a93441499c88f7034b9dea19f203b1d169689
Aigerim2309 <55251691+Aigerim2309@users.noreply.github.com>
2019-11-07 18:39:40 +0600

    Update script.js

--- script.js
+++ script.js
@@ -11,8 +11,22 @@ window.onload = function () {
                 display.value = "";
                 buffer.length = 0;
                 number.length = 0;
-            } 
+            } else if (this.value === "0" || this.value === "1" ||
+                this.value === "2" || this.value === "3" ||
+                this.value === "4" || this.value === "5" ||
+                this.value === "6" || this.value === "7" ||
+                this.value === "8" || this.value === "9") {
+                number += this.value;
+            } else {
+                buffer += number;
+                number = "";
+                
+                
+                
+                
+                
             display.value = number;
+            
         }
     }
 }
