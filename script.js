bb1546c15b60be44f34bb6a4bd35ef5c20c54276
Daryn <daryn07102001@gmail.com>
2019-11-09 04:09:03 +0600

    Some updates

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 let display = document.querySelector(".display");
 let buttons = document.querySelectorAll(".buttons input");
-let number = "";
-let buffer = "";
 
 window.onload = function () {
     for (let button of buttons) {
         button.onclick = function () {
-            display.value = "";
-            if (this.value === "C" || this.value === "c") {
-                display.value = "";
-                buffer.length = 0;
-                number.length = 0;
-            } 
-            display.value = number;
+            if (this.value === "0" || this.value === "1" ||
+                this.value === "2" || this.value === "3" ||
+                this.value === "4" || this.value === "5" ||
+                this.value === "6" || this.value === "7" ||
+                this.value === "8" || this.value === "9") {
+                display.value += this.value;
+            } else {
+                if (this.value === "C" || this.value === "c") {
+                    display.value = "";
+                } else if (this.value === "x") {
+                    display.value += this.value;
+                } else if (this.value === "√") {
+                    display.value += "**0.5";
+                } else if (this.value === "π") {
+                    display.value += "3.14";
+                } else if (this.value === "=") {
+                    display.value = display.value ? eval(display.value) : "";
+                } else if (this.value === ".") {
+                    display.value += ".";
+                } else {
+                    display.value += this.value;
+                }
+            }
         }
     }
 }
